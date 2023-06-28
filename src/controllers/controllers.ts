b9e8850e2a9999d3cb463175b9bd310555c47230
AHSATAL PIERRE <asathal.pierre@gmail.com>
2023-06-28 12:03:12 +0200

    setup routes, models and controllers

---
+++ src/controllers/controllers.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+import {  } from '../models/models';
+
+export const createTask = async (req: Request, res: Response) => {
+  
+  };
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,14 @@ import dotenv from 'dotenv';
 import { createConnection, Connection } from 'mysql2/promise';
 import bodyParser from 'body-parser';
 
+declare global {
+    namespace Express {
+      interface Request {
+        db: Connection;
+      }
+    }
+  }
+
 const app: Express = express();
 const port = process.env.PORT || 3000;
 dotenv.config();
@@ -11,13 +19,52 @@ dotenv.config();
 app.use(express.json());
 
 
-app.get('/', (req: Request, res: Response) => {
+  const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  };
+
+  const createDbConnection = async (): Promise<Connection> => {
+    try {
+      const connection = await createConnection(dbConfig);
+      console.log('Connected to the database');
+      return connection;
+    } catch (error) {
+      console.error('Error connecting to the database:', error);
+      throw error;
+    }
+  };
+
+  createDbConnection()
+  .then((connection) => {
+    app.use((req, res, next) => {
+      req.db = connection;
+      next();
+    });
+
+    app.use(bodyParser.json());
+
+    app.get('/', (req, res) => {
+      res.send('Hello, world!');
+    });
+
+    // app.use('/tasks', routes);
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Server initialization error:', error);
+  });
+
+
+  app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
   });
   
   app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
   });
-
-
-
